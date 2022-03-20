4d3cc03535297fb0d5c946632e9de6a3a1ec5420
Dominik Vach <vach.dominik@gmail.com>
2022-03-20 23:56:49 +0100

    add response printing

--- reqto/example.py
+++ reqto/example.py
@@ -8,7 +8,8 @@ def custom_function(parameter):
 package="reqto"
 response = reqto.get(f'https://pypi.org/pypi/{package}/json',timeout=5,timeout_function=custom_function,timeout_args="Timeout custom function called")
 """Will call timeout_function instead of raising an exception on Timeout"""
-
+print(response)
 
 response = reqto.get(f'https://pypi.org/pypi/{package}/json',timeout=1)
 """Will raise exception on Timeout"""
+print(response)
